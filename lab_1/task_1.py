c2faeea481c092d90f896a4e200417ca14feb0a9
Denys Morokov <morokov.denis@gmail.com>
2019-10-07 13:35:12 +0200

    mb done

--- lab_1/task_1.py
+++ lab_1/task_1.py
@@ -1,6 +1,41 @@
-def task_1():
-    print("1")
+def task_11():
+    for i in range(10):
+        if i == 1:
+            print("'''")
+            print("1")
+        elif i == 2:
+            print("22")
+        elif i == 3:
+            print("333")
+        elif i == 4:
+            print("4444")
+        elif i == 5:
+            print("55555")
+        elif i == 6:
+            print("666666")
+        elif i == 7:
+            print("7777777")
+        elif i == 8:
+            print("88888888")
+        elif i == 9:
+            print("999999999")
+            print("'''")
     return ''
+
+
+def task_1():
+    a = ("'''")
+    b = ("1")
+    c = ("22")
+    d = ("333")
+    e = ("4444")
+    f = ("55555")
+    g = ("666666")
+    h = ("7777777")
+    k = ("88888888")
+    l = ("999999999")
+    m = ("'''")
+    return print(a), print(b), print(c), print(d), print(e), print(f), print(g), print(h), print(k), print(l), print(m)
 
 
 assert task_1() == '''
@@ -14,5 +49,3 @@ assert task_1() == '''
 88888888
 999999999
 '''
-
-print("1")
