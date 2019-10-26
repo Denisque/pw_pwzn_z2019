15a62317fad223c04521a3e2c351bd5372ba8116
Denys Morokov <morokov.denis@gmail.com>
2019-10-26 15:41:05 +0200

    lab4: wykonany task_1

--- lab_4/tasks/task_1.py
+++ lab_4/tasks/task_1.py
@@ -3,11 +3,10 @@ Część 1 (1 pkt): Uzupełnij klasę Calculator
 tak by obsługiwała podstawowe operacje (podane jako string)
 oraz pamięć (memory, atrybut klasy) z interfejsem: dodaj do pamięci , wyczyść pamięć.
 Atrybut memory ma być nienadpisywalny.
+
 Część 2 (1 pkt): jeżeli drugi argument działania nie jest podany (None)
 użyj wartość z pamięci kalkulatora. Obsłuż przypadki skrajne.
 """
-
-# @propertis wykorzystac!!!!!!!!
 
 class Calculator:
     def __init__(self):
@@ -17,7 +16,7 @@ class Calculator:
         # wynik zapisany w tym atrybucie
         self._short_memory = None
 
-    def run(self, operator, arg1, arg2):
+    def run(self, operator, arg1, arg2=None):
         """
         Returns result of given operation.
 
@@ -30,15 +29,74 @@ class Calculator:
         :return: result of operation
         :rtype: float
         """
-        raise NotImplementedError
+        accept_operator = ["+", "-", "*", "/"]
+        if operator in accept_operator:
+
+            if arg2 != None:
+                if operator == '+':
+                    result = arg1 + arg2
+                    self._short_memory = result
+                    return result
+
+                if operator == '-':
+                    result = arg1 - arg2
+                    self._short_memory = result
+                    return result
+
+                if operator == '*':
+                    result = arg1 * arg2
+                    self._short_memory = result
+                    return result
+
+                if operator == '/':
+                    if arg2 != 0:
+                        result = arg1 / arg2
+                        self._short_memory = result
+                        return result
+                    if arg2 == 0:
+                        print("Nie mozna dzielic przez 0.")
+
+            if arg2 == None:
+                if self.memory != None:
+                    if operator == '+':
+                        result = arg1 + self.memory
+                        self._short_memory = result
+                        return result
+
+                    if operator == '-':
+                        result = arg1 - self.memory
+                        self._short_memory = result
+                        return result
+
+                    if operator == '*':
+                        result = arg1 * self.memory
+                        self._short_memory = result
+                        return result
+
+                    if operator == '/':
+                        if self.memory == 0:
+                            print("Nie mozna dzielic przez 0.")
+                        else:
+                            result = arg1 / self.memory
+                            self._short_memory = result
+                            return result
+
+                if self.memory == None:
+                    print("Brak zapamietanego wyniku, wiec nie mozna wykonac dzialanie")
+
+        else:
+            print("Podany operator nie jest dozwolony.\nPodaj operator jeszcze raz.")
+            print(f"Dozwolone opratory to: {accept_operator}.")
 
     def memorize(self):
         """Saves last operation result to memory."""
-        raise NotImplementedError
+        self.memory = self._short_memory
+        return self.memory
 
     def clean_memory(self):
         """Cleans memorized value"""
-        raise NotImplementedError
+        self._short_memory = None
+        self.memorize()
 
     def in_memory(self):
         """Prints memorized value."""
