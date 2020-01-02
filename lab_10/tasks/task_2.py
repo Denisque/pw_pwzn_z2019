6055ab5a50d17f9b9f25c0c4df622bb99d54984a
Denys Morokov <morokov.denis@gmail.com>
2020-01-02 23:52:08 +0100

    lab10: task_2 done

--- lab_10/tasks/task_2.py
+++ lab_10/tasks/task_2.py
@@ -1,5 +1,10 @@
 import pathlib
+import requests
+import calendar
+import csv
+
 from typing import Optional, Union, List
+from urllib.parse import urljoin
 
 
 API_URL = 'https://www.metaweather.com/api/'
@@ -10,7 +15,108 @@ def get_city_data(
         path: Optional[Union[str, pathlib.Path]] = None,
         timeout: float = 5.
 ) -> (str, List[str]):
-    pass
+
+    # 1. Jezeli brak sciezki
+    if path == None:
+        if month in range(10):
+            path = pathlib.Path.cwd() / f'{woeid}_{year}_0{month}'
+        else:
+            path = pathlib.Path.cwd() / f'{woeid}_{year}_{month}'
+        path.mkdir(parents=True, exist_ok=True)
+        monthRange = calendar.monthrange(year, month)
+        name_saved_files = []
+        for day in range(1, monthRange[1]+1):
+            location_url = urljoin(API_URL, f'location/{woeid}/{year}/{month}/{day}')
+            try:
+                response = requests.get(location_url, timeout=timeout)
+            except RuntimeError:
+                RuntimeError
+            try:
+                response = requests.get(location_url, timeout=timeout)
+            except requests.exceptions.Timeout:
+                requests.exceptions.Timeout
+            try:
+                response.status_code != 200
+            except requests.exceptions.HTTPError:
+                print(requests.exceptions.HTTPError)
+            response = requests.get(location_url, timeout=timeout)
+            data = response.json()
+            file_name = f'{year}_{month}_{day}.csv'
+            with open(path / file_name, 'w') as _file:
+                writer = csv.DictWriter(_file, delimiter=',', quotechar='"', fieldnames=data[0].keys())
+                writer.writeheader()
+                writer.writerows(data)
+            name_saved_files.append(file_name)
+        return str(path), name_saved_files
+
+    # 2. Podana jest ścieżka bezwzględna - nie edytuj ścieżki zapisu,
+    elif path[0] is '/':
+        path.mkdir(parents=True, exist_ok=True)
+        monthRange = calendar.monthrange(year, month)
+        name_saved_files = []
+        for day in range(1, monthRange[1] + 1):
+            print(day)
+            location_url = urljoin(API_URL, f'location/{woeid}/{year}/{month}/{day}')
+            try:
+                response = requests.get(location_url, timeout=timeout)
+            except RuntimeError:
+                RuntimeError
+            try:
+                response = requests.get(location_url, timeout=timeout)
+            except requests.exceptions.Timeout:
+                requests.exceptions.Timeout
+            try:
+                response.status_code != 200
+            except requests.exceptions.HTTPError:
+                print(requests.exceptions.HTTPError)
+            response = requests.get(location_url, timeout=timeout)
+            data = response.json()
+            file_name = f'{year}_{month}_{day}.csv'
+            with open(path / file_name, 'w') as _file:
+                writer = csv.DictWriter(_file, delimiter=',', quotechar='"', fieldnames=data[0].keys())
+                writer.writeheader()
+                writer.writerows(data)
+            name_saved_files.append(file_name)
+        return str(path), name_saved_files
+
+    # 3.podana jest ścieżka względna - miejscem zapisu jest podana ścieżka w folderze uruchomienia pliku
+    else:
+        if month in range(10):
+            path_out = path + '/' + f'{woeid}_{year}_0{month}'
+            path = pathlib.Path.cwd() / path / f'{woeid}_{year}_0{month}'
+        else:
+            path_out = path + '/' + f'{woeid}_{year}_{month}'
+            path = pathlib.Path.cwd() / path / f'{woeid}_{year}_{month}'
+        path.mkdir(parents=True, exist_ok=True)
+        monthRange = calendar.monthrange(year, month)
+        name_saved_files = []
+        for day in range(1, monthRange[1]+1):
+            location_url = urljoin(API_URL, f'location/{woeid}/{year}/{month}/{day}')
+            try:
+                response = requests.get(location_url, timeout=timeout)
+            except RuntimeError:
+                RuntimeError
+            try:
+                response = requests.get(location_url, timeout=timeout)
+            except requests.exceptions.Timeout:
+                requests.exceptions.Timeout
+            try:
+                response.status_code != 200
+            except requests.exceptions.HTTPError:
+                print(requests.exceptions.HTTPError)
+            response = requests.get(location_url, timeout=timeout)
+            data = response.json()
+            if data == []:
+                pass
+            else:
+                file_name = f'{year}_{month}_{day}.csv'
+                with open(path / file_name, 'w') as _file:
+                    writer = csv.DictWriter(_file, delimiter=',', quotechar='"', fieldnames=data[0].keys())
+                    writer.writeheader()
+                    writer.writerows(data)
+                name_saved_files.append(file_name)
+        return path_out, name_saved_files
+
 
 
 if __name__ == '__main__':
